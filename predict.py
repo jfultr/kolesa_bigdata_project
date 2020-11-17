5e2b859c415e74390bb9b048c2bb9474c9552ca4
Farkhat <farhatyakubov@gmail.com>
2020-11-17 19:49:21 +0600

    split function now replaced by split function with stratification;

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import hashlib
+
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def load_data(path):
@@ -11,17 +12,29 @@ def load_data(path):
         return pd.read_csv(path, encoding='ANSI')
 
 
-def test_set_check(identifier, test_ratio):
-    return hashlib.md5(np.int64(identifier)).digest()[-1] < 256 * test_ratio
-
-
 def split_train_test(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
+    strat_train_set = None
+    strat_test_set = None
+    ceil_k = 2500000  # Коэффицент маштабирования, чтобы ограничить количество страт
+    sever = 15000000  # Граница ценны автомобиля после которой все автомобили совмещаються в одну страту
+    data['_cat'] = np.ceil(data[id_column]/ceil_k)
+    data['_cat'].where(data['_cat'] < sever/ceil_k, sever/ceil_k, inplace=True)
+    split = StratifiedShuffleSplit(n_splits=1, test_size=test_ratio, random_state=42)
+
+    for train_index, test_index in split.split(data, data['_cat']):
+        strat_train_set = data.loc[train_index]
+        strat_test_set = data.loc[test_index]
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop('_cat', axis=1, inplace=True)
+
+    return strat_train_set, strat_test_set
 
 
 def get_predict(path):
     data = load_data(path).reset_index()
-    train_set, test_set = split_train_test(data, 0.2, 'index')
+    train_set, test_set = split_train_test(data, 0.2, 'price')
+
+
+
 
