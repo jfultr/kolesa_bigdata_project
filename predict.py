a2ce0684124aec01618974b9807475d44d181129
Farkhat <farhatyakubov@gmail.com>
2020-11-17 15:55:32 +0600

    set divided to train set and test set;

--- predict.py
+++ predict.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
 
 
 def load_data(path):
@@ -8,7 +11,17 @@ def load_data(path):
         return pd.read_csv(path, encoding='ANSI')
 
 
+def test_set_check(identifier, test_ratio):
+    return hashlib.md5(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+
+def split_train_test(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
 def get_predict(path):
-    data = load_data(path)
-    print(data.iloc[1])
+    data = load_data(path).reset_index()
+    train_set, test_set = split_train_test(data, 0.2, 'index')
 
