a21ba76e40974f8e61ef7f0a189cc052c40069b5
Farkhat <farhatyakubov@gmail.com>
2020-11-18 12:33:36 +0600

    added some plots drawing code;

--- predict.py
+++ predict.py
@@ -19,6 +19,7 @@ def split_train_test(data, test_ratio, id_column):
     sever = 15000000  # Граница ценны автомобиля после которой все автомобили совмещаються в одну страту
     data['_cat'] = np.ceil(data[id_column]/ceil_k)
     data['_cat'].where(data['_cat'] < sever/ceil_k, sever/ceil_k, inplace=True)
+
     split = StratifiedShuffleSplit(n_splits=1, test_size=test_ratio, random_state=42)
 
     for train_index, test_index in split.split(data, data['_cat']):
@@ -31,10 +32,20 @@ def split_train_test(data, test_ratio, id_column):
     return strat_train_set, strat_test_set
 
 
+def draw_plots_to_research_data(data):  # just to draw some plots
+    # 1
+    data.plot(kind='scatter', x='year', y='price', alpha=0.1)
+    plt.show()
+    # 2
+    most_pop_cars = data['name'].value_counts()
+    most_pop_cars = pd.DataFrame(data={'count': most_pop_cars.values},
+                                 index=most_pop_cars.index)
+    most_pop_cars.iloc[:40].plot(kind='pie', y='count', figsize=(15, 6))
+    plt.show()
+
+
 def get_predict(path):
     data = load_data(path).reset_index()
     train_set, test_set = split_train_test(data, 0.2, 'price')
-
-
 
 
